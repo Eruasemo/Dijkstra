1464ab121f6694f7a04ddbe8f8538c15d935f86d
MANUEL BALDERAS VICTORIA <manuel.balderas@edu.uag.mx>
2024-07-15 17:09:37 -0600

    Agregar archivos de proyecto.

---
+++ Grafo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    internal class Grafo
+    {
+        private int vertices;
+        private int[,] matriz;
+
+        public Grafo (int vertices)
+        {
+            this.vertices = vertices;
+            this.matriz = new int[vertices, vertices];
+        }
+
+        public void CrearArista(int origen, int destino, int costo, bool orden)
+        {
+            matriz[origen, destino] = costo;
+            if (!orden)
+            {
+                matriz[destino,origen] = costo;
+            }
+        }
+
+        private int distanciaMin(int[] distancia,
+                   bool[] nodos)
+        {
+            // Initialize min value
+            int min = int.MaxValue, min_index = -1;
+
+            for (int i = 0; i < vertices; i++)
+                if (nodos[i] == false && distancia[i] <= min)
+                {
+                    min = distancia[i];
+                    min_index = i;
+                }
+
+            return min_index;
+        }
+
+        private void imprimir(int[] distancia)
+        {
+            Console.WriteLine("Vertice \t Distancia desde origen");
+            for (int i = 0; i < vertices; i++)
+                Console.WriteLine(i + "\t\t" + distancia[i]);
+        }
+
+        public void Dijkstra(int origen)
+        {
+            bool[] nodos = new bool[vertices];
+            int[] distancia = new int[vertices];
+
+            for(int i = 0; i < vertices; i++)
+            {
+                distancia[i] = int.MaxValue;
+                nodos[i] = false;
+            }
+
+            distancia[origen] = 0;
+
+            for(int i = 0; i < vertices-1; i++)
+            {
+                int u = distanciaMin(distancia, nodos);
+
+                nodos[u] = true;
+
+                for(int j = 0; j < vertices; j++)
+                {
+                    if (!nodos[j] && matriz[u,j] != 0&&
+                        distancia[u] != int.MaxValue && distancia[u]+ matriz[u,j] < distancia[j])
+                    {
+                        distancia[j] = distancia[u] + matriz[u,j];
+                    }
+                }
+            }
+            imprimir(distancia);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dijkstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Grafo test = new Grafo(9);
+            /*
+            test.matriz = new int[,]{
+                                      { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+            };*/
+
+            test.CrearArista(0, 1, 4, false);
+            test.CrearArista(0, 7, 8, false);            
+            test.CrearArista(1, 2, 8, false);
+            test.CrearArista(1, 7, 11, false);
+            test.CrearArista(2, 3,7, false);
+            test.CrearArista(2, 5, 4, false);
+            test.CrearArista(2, 8, 2, false);
+            test.CrearArista(3,4,9,false);
+            test.CrearArista(3,5,14,false);
+            test.CrearArista(4, 5, 10, false);
+            test.CrearArista(5, 6, 2, false);
+            test.CrearArista(6,7,1,false);
+            test.CrearArista(6, 8, 6, false);
+            test.CrearArista(7, 8, 7, false);
+
+            test.Dijkstra(0);
+        }
+    }
+}
